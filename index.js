79747d1a63d53e44bd78bc5029927517e1624b79
lucio <luciocarvalhodev@gmail.com>
2021-11-23 21:12:20 -0300

    criado loop principal, execução ainda apresenta falhas

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const SIZE = 5;
+console.log('---');
+
+const SIZE = 6;
 const MARGIN = 1;
 
 function createGrid(size = SIZE) {
@@ -10,6 +12,9 @@ function createGrid(size = SIZE) {
             copy[x][y] = `${copy[x][y]}`;
             return copy;
         },
+        get: (line, col) => {
+            return Array.from(grid.area[line][col]);
+        },
         getNeighborsFromCell: ([line, col]) => {
             let count = 0;
             grid._neighboardsPositions.forEach(([posX, posY]) => {
@@ -20,9 +25,9 @@ function createGrid(size = SIZE) {
             });
             return count;
         },
-        changeCell: (...list) => {
+        changeCell: (state, ...list) => {
             list.forEach(([line, col]) =>
-                grid.area[line][col] = +!grid.area[line][col]);
+                grid.area[line][col] = state);
         },
         _neighboardsPositions: [
             [-1, -1],
@@ -45,4 +50,42 @@ function createGrid(size = SIZE) {
     return grid;
 }
 
-const grid = createGrid();
+
+function step(grid) {
+    // caso todas as celulas estejam mortas não faz nada
+    if (grid.area.flat(Infinity).indexOf(1) == -1)
+        return true;
+
+    const newGrid = Object(grid);
+
+    // calcula o novo estado de cada celula
+    grid.area.forEach((line, lineIdx) => {
+        line.forEach((col, colIdx) => {
+            const cell = [lineIdx, colIdx];
+            const neigh = grid.getNeighborsFromCell(cell);
+
+            // caso "viva"
+            if (neigh > 3 || neigh < 3) {
+                newGrid.changeCell(0, cell);
+            } else {
+                newGrid.changeCell(1, cell);
+            }
+
+        });
+    });
+    grid.area = newGrid.area;
+
+    return false;
+}
+
+const grid5 = createGrid();
+grid5.changeCell(1, [2, 2], [3, 3], [4, 2], [3, 1], [3, 2]);
+
+console.log(grid5.area);
+
+const id = setInterval(() => {
+    const stop = step(grid5);
+    console.log(grid5.area);
+    if (stop)
+        clearInterval(id);
+}, 1000);
